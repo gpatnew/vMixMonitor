48ee77b37ce9843fdca1b70919fe06df41b7bb6a
Pat Newsome <gpatnew@hotmail.com>
2020-08-27 16:34:16 -0700

    Add Logging serilog

--- vMixMonitor/Helpers/vMixHelper.cs
+++ vMixMonitor/Helpers/vMixHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -107,6 +108,7 @@ namespace vMixMonitor
             {
                 doc = null;
                 onLine = false;
+                Log.Error(e, "Error");
             }
             
            
